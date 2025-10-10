ecf4a8c3b418496ef8b813fa09631ad5362c8e18
touyamayusuke <yusuke2710@icloud.com>
2025-10-10 14:26:33 +0900

    Postモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :nickname, presence: :true, length: { maximum: 20 }
 end
