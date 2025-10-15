e28b23202ab5ac2b2f4678560e2df1b94724b7c2
touyamayusuke <yusuke2710@icloud.com>
2025-10-15 13:01:09 +0900

    ユーザーマイページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
-  resources :posts, only: [:index, :show, :new, :create, :destroy] 
+  resources :posts, only: [:index, :show, :new, :create, :destroy]
+  resources :users, only: [:show]
 end
